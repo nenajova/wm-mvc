4bb4756808ba1ab52e4d665ae7e500600cb1308f
Nenad Jovanovic <nenajova@gmail.com>
2018-07-13 16:32:58 +0200

    Basic routing and pages

--- wm-mvc/wm-mvc/Controllers/HomeController.cs
+++ wm-mvc/wm-mvc/Controllers/HomeController.cs
@@ -14,22 +14,11 @@ namespace wm_mvc.Controllers
         {
             return View();
         }
-
-        public IActionResult About()
+        public IActionResult Json()
         {
-            ViewData["Message"] = "Your application description page.";
-
             return View();
         }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Privacy()
+        public IActionResult Database()
         {
             return View();
         }
